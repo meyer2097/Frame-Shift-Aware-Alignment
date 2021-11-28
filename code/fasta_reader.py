a86c04b830c460b3ecf4cc9072dcb35076d6b517
Jules Kreuer <contact@juleskreuer.eu>
2021-11-28 18:49:09 +0100

    additional fasta_object functions

--- code/fasta_reader.py
+++ code/fasta_reader.py
@@ -1,9 +1,8 @@
 """
-@author: Clara Heimburger, Jules Kreuer
+@author: Jules Kreuer
 """
 
 from os import path
-
 
 class fasta_object():
     def __init__(self, head, body):
@@ -13,74 +12,95 @@ class fasta_object():
         self.head = head
         self.body = body
 
-
-class fasta_handler():
-    def read(self, fileName):
+    def __str__(self): 
         """
-        Reads a fasta-style file and returns a list of fasta_objects
+        Magic method to allow fasta_object printing.
         """
+        out = f'{self.head}\n'
 
-        if not path.isfile(fileName):
-            raise FileNotFoundError("FastaFile not found!")
+        # Print only 70 chars per line
+        for i in range(0, len(self.body), 70):
+            out += f'{self.body[i:i+70]}\n'
+        return out[:-1] # Remove tailing newline
 
-        fasta_objects = []
-        with open(fileName, 'r') as f:
-            head = ""
-            body = ""
-            newObject = True
-            for line in f:
-                # go through each line
-                # First Header
-                if newObject and line.startswith(">"):
-                    head = line.strip()
-                    body = ""
-                    newObject = False
-                # N-th Header
-                elif line.startswith(">"):
-                    fasta_objects.append(fasta_object(head, body))
-                    head = line.strip()
-                    body = ""
-                # Sequence
-                else:
-                    body += line.strip().upper()
-            # append last element
-            fasta_objects.append(fasta_object(head, body))
-        return fasta_objects
-
-    def write(self, fasta_pairs, fileName, mode="w"):
+    def __repr__(self):
         """
-        Writes a list of fasta_objects or a single one to a file.
-        Takes fasta_objects as input.
+        Magic method to allow printing of fasta_object representation.
         """
+        return f'fasta_object("{self.head}", "{self.body}")'
 
-        if not isinstance(fasta_pairs, list):
-            fasta_pairs = [fasta_pairs]
-
-        with open(fileName, mode) as f:
-            for fo in fasta_pairs:
-                f.write(fo.head + "\n")
-                body_len = len(fo.body)
-                # Write only 70 chars per line
-                for i in range(0, body_len, 70):
-                    f.write(fo.body[i:i+70] + "\n")
-        return None
-
-
-    def print(self, fasta):
+    def __eq__(self, o):
         """
-        Prints a single or a list of fasta_objects.
-        Takes fasta_objects as input.
+        Magic method to allow equality check on fasta_objects
         """
+        return self.head == o.head and self.body == o.body
 
-        if not isinstance(fasta, list):
-            fasta = [fasta]
-        
-        for fo in fasta:
-            print(fo.head)
+def read_fasta(self, fileName):
+    """
+    Reads a fasta-style file and returns a list of fasta_objects
+    """
+
+    if not path.isfile(fileName):
+        raise FileNotFoundError("FastaFile not found!")
+
+    fasta_objects = []
+    with open(fileName, 'r') as f:
+        head = ""
+        body = ""
+        newObject = True
+        for line in f:
+            # go through each line
+            # First Header
+            if newObject and line.startswith(">"):
+                head = line.strip()
+                body = ""
+                newObject = False
+            # N-th Header
+            elif line.startswith(">"):
+                fasta_objects.append(fasta_object(head, body))
+                head = line.strip()
+                body = ""
+            # Sequence
+            else:
+                body += line.strip().upper()
+        # append last element
+        fasta_objects.append(fasta_object(head, body))
+    return fasta_objects
+
+def write_fasta(self, fasta_pairs, fileName, mode="w"):
+    """
+    Writes a list of fasta_objects or a single one to a file.
+    Takes fasta_objects as input.
+    """
+
+    if not isinstance(fasta_pairs, list):
+        fasta_pairs = [fasta_pairs]
+
+    with open(fileName, mode) as f:
+        for fo in fasta_pairs:
+            f.write(fo.head + "\n")
             body_len = len(fo.body)
-            # Print only 70 chars per line
+            # Write only 70 chars per line
             for i in range(0, body_len, 70):
-                print(fo.body[i:i+70])
-        return None
+                f.write(fo.body[i:i+70] + "\n")
+    return True
+
+
+def print_fasta(self, fasta):
+    """
+    Prints a single or a list of fasta_objects.
+    Takes fasta_objects as input.
+    """
+
+    if not isinstance(fasta, list):
+        fasta = [fasta]
+    
+    for fo in fasta:
+        print(fo.head)
+        body_len = len(fo.body)
+        # Print only 70 chars per line
+        for i in range(0, body_len, 70):
+            print(fo.body[i:i+70])
+    return None
              
             
