8a28b33308eb5924c20213021b4d4d67097be272
Jules Kreuer <contact@juleskreuer.eu>
2021-12-04 18:35:02 +0100

    generalize filepath in tests

--- code/tests/test_fasta.py
+++ code/tests/test_fasta.py
@@ -1,19 +1,27 @@
 import pytest
 import fasta
-
+from os import path, remove
 
 def test_read_fasta():
-    assert fasta.read_fasta("read_fasta_test.fasta") == [
+    fp = path.join(path.dirname(__file__), "read_fasta_test.fasta")
+    assert fasta.read_fasta(fp) == [
         fasta.fasta_object(">Atlantic dolphin", "CGGCCTTCTATCTTCTTC"),
         fasta.fasta_object(">Pacific dolphin", "CTTTCTATCTCTTTCCTCT")]
 
 
-def test_write_fasta():
-    fasta.write_fasta([fasta.fasta_object(">Atlantic dolphin", "CGGCCTTCTATCTTCTTC"),
-                       fasta.fasta_object(">Pacific dolphin", "CTTTCTATCTCTTTCCTCT")], "write_fasta_test.fasta")
-    file_test = open("write_fasta_test.fasta", "r")
-    file = open("read_fasta_test.fasta", "r")
-    assert file_test.read() == file.read() + "\n"
+def test_write_read_fasta():
+
+    file_path = path.join(path.dirname(__file__), "write_fasta_test.fasta")
+
+    fo = [fasta.fasta_object(">Atlantic dolphin", "CGGCCTT*CTAAAAATTZZZ*ZZZZASASD*TCTTCTTC"),
+          fasta.fasta_object(">Pacific dolphin", "CTTTCTATCTCSATTTCCTCT")]
+    
+    fasta.write_fasta(fo, file_path)
+    
+    fo_read = fasta.read_fasta(file_path)
+    remove(file_path)
+    assert fo == fo_read
+    
 
 
 def test_translate_seq():
@@ -22,7 +30,10 @@ def test_translate_seq():
 
 
 def test_print_fasta(capsys):
-    assert fasta.print_fasta(fasta.read_fasta("read_fasta_test.fasta")) is None
+    file_path = path.join(path.dirname(__file__), "read_fasta_test.fasta")
+
+    assert fasta.print_fasta(fasta.read_fasta(file_path)) is None
+
     # check if it prints the sequences correctly
     captured = capsys.readouterr()
     assert captured.out == ">Atlantic dolphin" + "\n" + "CGGCCTTCTATCTTCTTC" + "\n" + ">Pacific dolphin" + "\n" + "CTTTCTATCTCTTTCCTCT" + "\n"
