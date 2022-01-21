5c06acadc0c55f72a7c4d6849165b0bbe75aebe1
Jules Kreuer <contact@juleskreuer.eu>
2022-01-21 11:15:02 +0100

    add docstring

--- src/frameshift_aware_alignment/__init__.py
+++ src/frameshift_aware_alignment/__init__.py
@@ -1,3 +1,6 @@
+"""
+Frameshift aware Needleman-Wunsch for DNA and AA-Sequences.
+"""
 from .frameshift_aware_alignment import align
 from .fasta import fasta_object, read_fasta, write_fasta, print_fasta, translate_seq
 from .blosum import BLOSUM
