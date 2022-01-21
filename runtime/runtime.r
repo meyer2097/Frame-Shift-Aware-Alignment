d575ff67d8482b60861b0c61e599f35361c073a3
Jules Kreuer <contact@juleskreuer.eu>
2022-01-21 18:21:09 +0100

    add runtime analysis

---
+++ runtime/runtime.r
@@ -0,0 +1,13 @@
+library(reshape2)
+d = read.csv("median.csv", header = TRUE)
+meltedData = melt(d, id.vars = "X")
+names(meltedData) = c("DNA", "AA", "Time")
+
+meltedData["AA"] = lapply(meltedData["AA"], function(x) substring(x, 2))
+meltedData$Time = meltedData$Time*1000
+meltedData$AA = as.numeric(meltedData$AA)
+meltedData$DNA = as.numeric(meltedData$DNA)
+
+meltedData$DNAtimesAA = meltedData$DNA * meltedData$AA
+
+l = lm(Time ~ DNAtimesAA, data = meltedData)
